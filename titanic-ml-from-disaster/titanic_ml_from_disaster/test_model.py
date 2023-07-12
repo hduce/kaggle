cfae9d8c54ad38346b5be07521c0f3b4f9b8d0b5
hduce <harry.duce@gmail.com>
2023-07-12 22:49:44 +0100

    add pclass as feature

---
+++ titanic-ml-from-disaster/titanic_ml_from_disaster/test_model.py
@@ -0,0 +1,3 @@
+"""TODO - testing requires the same preparation on the test dataset
+
+Need to find sensible ways to keep code modular."""
--- titanic-ml-from-disaster/titanic_ml_from_disaster/train_xgboost.py
+++ titanic-ml-from-disaster/titanic_ml_from_disaster/train_xgboost.py
@@ -9,15 +9,15 @@ _MODELS = Path(__file__).parent.parent / "models"
 
 
 def train_xgboost(train: pl.DataFrame) -> None:
-    x = train.select(pl.col("*").exclude("Label"))
-    y = train.select(pl.col("Label"))
+    x = train.select(pl.col("*").exclude("Label")).to_numpy()
+    y = train.select(pl.col("Label")).to_numpy()
 
     print(f"X input shape - {x.shape}")
     print(f"y labels shape - {y.shape}")
-    print(f"X.head(3) - {x.head(3)}")
-    print(f"y.head(3) - {y.head(3)}")
+    print(f"X[:3] - {x[:3]}")
+    print(f"y[:3] - {y[:3]}")
 
-    model = xgb.XGBClassifier()
+    model: xgb.sklearn.XGBClassifier = xgb.XGBClassifier()
 
     model.fit(x, y)
 
@@ -25,11 +25,13 @@ def train_xgboost(train: pl.DataFrame) -> None:
     accuracy = accuracy_score(y, y_pred)
     print(f"Module accuracy on training set - {accuracy * 100:.2f}%")
 
-    model_f_path = _MODELS / "model_features_sex_agestatus.json"
+    model_f_path = _MODELS / "model_features_sex_agestatus_pclass.json"
     print(f"Saving model to {model_f_path}")
     model.save_model(model_f_path)
 
 
 if __name__ == "__main__":
-    training_data = pl.read_csv(_TRANSFORMED / "train_features_sex_agestatus.csv")
+    training_data = pl.read_csv(
+        _TRANSFORMED / "train_features_sex_agestatus_pclass.csv"
+    )
     train_xgboost(training_data)
