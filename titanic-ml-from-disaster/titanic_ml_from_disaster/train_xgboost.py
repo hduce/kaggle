9e9828fffe8ee21c831b30857894f8df8b8b53e5
hduce <harry.duce@gmail.com>
2023-07-12 21:58:46 +0100

    organise project
    * train_xgboost.py for training and saving
    * prepare_train_features_sex.py for prep & saving of training ready data
    * slim down notebook to just be exploratory

---
+++ titanic-ml-from-disaster/titanic_ml_from_disaster/train_xgboost.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+import polars as pl
+
+_TRANSFORMED = Path(__file__).parent.parent / "data" / "transformed"
+_MODELS = Path(__file__).parent.parent / "models"
+
+
+def train_xgboost(train: pl.DataFrame) -> None:
+    x = train.select(pl.col("*").exclude("Label"))
+    y = train.select(pl.col("Label"))
+
+    print(f"X input shape - {x.shape}")
+    print(f"y labels shape - {y.shape}")
+    print(f"X.head(3) - {x.head(3)}")
+    print(f"y.head(3) - {y.head(3)}")
+
+    model = xgb.XGBClassifier()
+
+    model.fit(x, y)
+
+    y_pred = model.predict(x)
+    accuracy = accuracy_score(y, y_pred)
+    print(f"Module accuracy on training set - {accuracy * 100:.2f}%")
+
+    model_f_path = _MODELS / "model_features_sex.json"
+    print(f"Saving model to {model_f_path}")
+    model.save_model(model_f_path)
+
+
+if __name__ == "__main__":
+    training_data = pl.read_csv(_TRANSFORMED / "train_features_sex.csv")
+    train_xgboost(training_data)
